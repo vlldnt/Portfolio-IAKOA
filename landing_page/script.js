51f768af67b4d2681137346e79426547c788dd5a
vlldnt <vieilledent.adrien@gmail.com>
2025-07-02 15:58:11 +0200

    feat: enhance landing page with animations and improved header visibility

--- landing_page/script.js
+++ landing_page/script.js
@@ -1,6 +1,6 @@
 AOS.init({
-  duration: 1200, 
-  once: false 
+  duration: 1200,
+  once: false,
 });
 
 window.addEventListener("scroll", () => {
@@ -8,30 +8,35 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     showHeader.classList.remove("-translate-y-full");
     showHeader.classList.add("translate-y-0");
+    showHeader.style.visibility = "visible";
   } else {
     showHeader.classList.remove("translate-y-0");
     showHeader.classList.add("-translate-y-full");
+    showHeader.style.visibility = "hidden";
   }
 });
 
 function scrollToSectionFeatures() {
-  const header = document.getElementById('mainHeader');
-  const target = document.getElementById('features');
+  const header = document.getElementById("mainHeader");
+  const target = document.getElementById("features");
   const headerHeight = header.offsetHeight;
   if (target) {
     const top = target.offsetTop - headerHeight;
-    window.scrollTo({ top, behavior: 'smooth' });
-    location.hash = "#features"
+    window.scrollTo({ top, behavior: "smooth" });
+    location.hash = "#features";
   }
 }
 
 function scrollToSectionPro() {
-  const header = document.getElementById('mainHeader');
-  const target = document.getElementById('pro');
+  const header = document.getElementById("mainHeader");
+  const target = document.getElementById("pro");
   const headerHeight = header.offsetHeight;
   if (target) {
     const top = target.offsetTop - headerHeight;
-    window.scrollTo({ top, behavior: 'smooth' });
-    location.hash = "#pro"
+    window.scrollTo({ top, behavior: "smooth" });
+    location.hash = "#pro";
   }
 }
+
+
+
